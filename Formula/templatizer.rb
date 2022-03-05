a56d7745e9e801e61af12995bbefdf5a58592dcb
Iván Corrales Solera <ivan.corrales.solera@gmail.com>
2022-03-05 19:06:56 +0100

    Update

---
+++ Formula/templatizer.rb
@@ -0,0 +1,20 @@
+class Templatizer < Formula
+    desc "A simple wrapper to kubectl port-forward for multiple pods."
+    homepage "https://github.com/wesovilabs/templatizer"
+    url "https://github.com/wesovilabs/templatizer/archive/v0.0.1.tar.gz"
+    #sha256 "7852a5500f3e35a47b57d138c756de5641bc3c48bf7e329d2724c1107ccb1207"
+  
+    depends_on "go"
+  
+    def install
+      ENV["GOPATH"] = buildpath
+      ENV["GO111MODULE"] = "on"
+      ENV["GOFLAGS"] = "-mod=vendor"
+      ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
+      (buildpath/"src/github.com/wesovilabs/templatizer").install buildpath.children
+      cd "src/github.com/wesovilabs/templatizer" do
+        system "go", "build", "-o", bin/"templatizer", "."
+      end
+    end
+
+  end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Overview
+Wesovilabs homebrew tap for custom formulas.
+
+# Installation
+## Add tap to your Homebrew:
+$ brew tap wesovilabs/tool
+
+##  Install tool:
+$ brew install <tool>
